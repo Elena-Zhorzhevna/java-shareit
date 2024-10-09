09ec2f0b5c3f71ab1d0e3332a679a41ec889ac28
zhorzhevnaElena <zhorzhevna.elena@yandex.ru>
2024-10-09 23:52:35 +0300

    Добавлены тесты для классов UserController и UserMapper в модуле shareIt-server.

--- shareIt-server/src/main/java/ru/practicum/shareit/server/user/dto/UserDto.java
+++ shareIt-server/src/main/java/ru/practicum/shareit/server/user/dto/UserDto.java
@@ -3,11 +3,12 @@ package ru.practicum.shareit.server.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
 
 /**
  * Класс представляет объект, который будет возвращать сервис при запросе данных о пользователе.
  */
+@NoArgsConstructor
 @Data
 public class UserDto {
     private Long id;
@@ -17,4 +18,10 @@ public class UserDto {
     @NotNull
     @NotBlank
     private String email;
+
+    public UserDto(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
